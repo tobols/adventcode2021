797350a589448c3d3e125b18d68345bde3d50529
Tobias Olsson <tobias.olsson@isotop.se>
2021-12-01 11:06:45 +0100

    here we go again

---
+++ 001/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = ReadFile().ToArray();
+            var sumDeeper = 0;
+
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                if (values[i] < values[i + 1])
+                    sumDeeper++;
+            }
+
+            Console.WriteLine($"Sum deeper measurements: {sumDeeper}");
+
+
+            var sumDeeperWindow = 0;
+
+            for (int i = 1; i < values.Length - 2; i++)
+            {
+                var windowSum = values[(i-1)..(i+2)].Sum();
+                var nextWindow = values[(i)..(i+3)].Sum();
+                if (nextWindow > windowSum)
+                    sumDeeperWindow++;
+            }
+
+            Console.WriteLine($"Sum deeper window measurements: {sumDeeperWindow}");
+        }
+
+
+        private static IEnumerable<int> ReadFile()
+        {
+            var file = new System.IO.StreamReader(@"input.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+                yield return Convert.ToInt32(line);
+            file.Close();
+        }
+    }
+}
